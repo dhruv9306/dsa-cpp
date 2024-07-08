3c6cabcc0b244bfb023a4ef2b249663cd4ad53b3
Dhruv Gandhi <dhruvgandhi870842@gmail.com>
2024-07-09 00:17:23 +0530

    new pattern

--- patterns.cpp
+++ patterns.cpp
@@ -51,8 +51,31 @@ void pattern3 (int n){
 
     }
 }
+void pattern4(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        int breakpoint=(2*i+1)/2;
+        char ch='A';
+        for(int j=1;j<=2*i+1;j++){
+            cout<<ch;
+            if(j<=breakpoint){
+                ch++;
+            }
+            else{
+                ch--;
+            }
+
+        }
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n;
     cin>>n;
-    pattern3(n); //your function (pattern 1,2,3,4(n))
+    pattern4(n); //your function (pattern 1,2,3,4(n))
 }
