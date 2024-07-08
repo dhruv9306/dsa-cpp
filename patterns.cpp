dfee8acd22b763d9a462797e1756dd22820dff69
Dhruv Gandhi <dhruvgandhi870842@gmail.com>
2024-07-08 15:27:24 +0530

    patterns

--- patterns.cpp
+++ patterns.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-void pattern(int n){
+void pattern1(int n){
     for(int i=1;i<=2*n-1;i++){
         int k=i;
         if(i>n){
@@ -13,8 +13,28 @@ void pattern(int n){
     }
     
 }
+void pattern2(int n){
+    int start;
+    for(int i=0;i<n;i++){
+        
+        if(i%2==0){
+            start=1;
+        }
+        else{
+            start=0;
+        }
+        for(int j=0;j<=i;j++){
+            cout<<start<<" ";
+            start=1-start;
+
+        }
+        cout<<endl;
+
+
+    }
+}
 int main(){
     int n;
     cin>>n;
-    pattern(n);
+    pattern2(n); //your function (pattern 1,2,3,4(n))
 }
