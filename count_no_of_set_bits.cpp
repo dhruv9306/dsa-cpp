68a42872ff66f9890efbdf4027cd9d68f0ebf50d
Dhruv Gandhi <dhruvgandhi870842@gmail.com>
2024-07-13 00:43:42 +0530

    change spme code

--- count_no_of_set_bits.cpp
+++ count_no_of_set_bits.cpp
@@ -3,11 +3,12 @@ using namespace std;
 int countsetbits(int n){
     int cnt=0;
     while(n>=1){
-        if(n%2==1){
-            cnt++;
-        }
-        n=n/2;
-    }
+        // if(n%2==1){
+        //     cnt++;
+        // }
+        cnt+=n&1; //alternative methdod by bitwise operation and
+        n=n>>1; // right shift
+    } 
     return cnt;
 }
 int main(){
